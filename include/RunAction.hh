9cd76c65fc97c61116bfa32789974bdb958aa4d5
ypiadyk <yurii.piadyk@gmail.com>
2014-04-13 15:45:57 +0300

    Added Blank RunAction Class

---
+++ include/RunAction.hh
@@ -0,0 +1,12 @@
+#ifndef RUNACTION_HH
+#define RUNACTION_HH
+
+#include "G4UserRunAction.hh"
+
+class RunAction : public G4UserRunAction
+{
+public:
+    RunAction();
+};
+
+#endif // RUNACTION_HH
---
+++ src/RunAction.cc
@@ -0,0 +1,5 @@
+#include "RunAction.hh"
+
+RunAction::RunAction()
+{
+}
