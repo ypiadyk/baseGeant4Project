0d953b2503eb3e333aa9f8f4e98196935322331d
ypiadyk <yurii.piadyk@gmail.com>
2014-04-13 21:07:00 +0300

    New line

--- src/RunAction.cc
+++ src/RunAction.cc
@@ -1,7 +1,9 @@
 #include "RunAction.hh"
+#include "TFile.h"
 
 RunAction::RunAction()
 {
+
 }
 
 RunAction::~RunAction()
@@ -11,10 +13,13 @@ RunAction::~RunAction()
 
 void RunAction::BeginOfRunAction(const G4Run *)
 {
-
+    hist = new TH1D("hist", "hist", 1000, 0, 3000);
 }
 
 void RunAction::EndOfRunAction(const G4Run *)
 {
-
+    TFile* f = new TFile("spectrum.root", "recreate");
+    hist->Write();
+    f->Write();
+    // new line
 }
